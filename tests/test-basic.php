7c09398fafd4b16df872829bcf0651656dd8c9fd
fumikito <guy@hametuha.com>
2017-01-08 13:42:52 +0900

    Fix again

--- tests/test-basic.php
+++ tests/test-basic.php
@@ -10,21 +10,33 @@
  */
 class NLMG_Basic_Test extends WP_UnitTestCase {
 
-	protected $backupGlobals = false;
+	protected $backupGlobals = true;
 
 	/**
 	 * A single example test
 	 *
-	 * @runInSeparateProcess
 	 */
-	function test_delete_user() {
+	function test_auto_loader() {
 		// Check class exists
 		$this->assertTrue( class_exists( 'NeverLetMeGo\\Admin' ) );
 		$this->assertTrue( class_exists( 'NeverLetMeGo\\Page' ) );
-		// Create user
+	}
+
+	/**
+	 * Delete user
+	 */
+	protected function delete_single_user() {
 		$user = nlmg_pseudo_user();
 		$this->assertInstanceOf( 'WP_User', $user );
 		$result = \NeverLetMeGo\Admin::getInstance()->delete_user( $user->ID );
 		$this->assertTrue( $result );
+	}
+
+	/**
+	 * Delete user up to 4.7
+	 * @runInSeparateProcess
+	 */
+	function test_delete_user() {
+		$this->delete_single_user();
 	}
 }
