91066058dba688e714ab5f1c43399911adaf11a9
fumikito <guy@hametuha.com>
2017-01-08 13:16:49 +0900

    Put in 1 method.

--- tests/test-basic.php
+++ tests/test-basic.php
@@ -18,19 +18,13 @@ class NLMG_Basic_Test extends WP_UnitTestCase {
 	 * @runInSeparateProcess
 	 */
 	function test_delete_user() {
+		// Check class exists
+		$this->assertTrue( class_exists( 'NeverLetMeGo\\Admin' ) );
+		$this->assertTrue( class_exists( 'NeverLetMeGo\\Page' ) );
 		// Create user
 		$user = nlmg_pseudo_user();
 		$this->assertInstanceOf( 'WP_User', $user );
 		$result = \NeverLetMeGo\Admin::getInstance()->delete_user( $user->ID );
 		$this->assertTrue( $result );
-	}
-
-	/**
-	 * Check auto loader
-	 */
-	function test_auto_loader() {
-		// Check class exists
-		$this->assertTrue( class_exists( 'NeverLetMeGo\\Admin' ) );
-		$this->assertTrue( class_exists( 'NeverLetMeGo\\Page' ) );
 	}
 }
