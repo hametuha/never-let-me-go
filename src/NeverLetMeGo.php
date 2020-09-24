0bf60140316a3e5a0ffea04355f9388d7a2a3799
fumikito <guy@hametuha.com>
2020-09-24 21:43:23 +0900

    Add user import command.

--- src/NeverLetMeGo.php
+++ src/NeverLetMeGo.php
@@ -13,9 +13,14 @@ class NeverLetMeGo extends \NeverLetMeGo\Pattern\Application {
 	 * @param array $settings
 	 */
 	public function __construct( array $settings = [] ) {
+		// Initialize instances.
 		\NeverLetMeGo\Admin::getInstance();
 		\NeverLetMeGo\Page::getInstance();
 		\NeverLetMeGo\WooCommerce::getInstance();
+		// Register command if avilable.
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			WP_CLI::add_command( 'nlmg', \NeverLetMeGo\Command::class );
+		}
 	}
 	
 	
